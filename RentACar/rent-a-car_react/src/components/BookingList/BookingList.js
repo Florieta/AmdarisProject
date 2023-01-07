eabd5eedb2e5f92373e0ae5384d6410699a28dd0
Florieta <florieta.pencheva@gmail.com>
2023-01-07 21:05:43 +0200

    Amend my bookings list

--- RentACar/rent-a-car_react/src/components/BookingList/BookingList.js
+++ RentACar/rent-a-car_react/src/components/BookingList/BookingList.js
@@ -1,20 +1,78 @@
-import BookingCard from "./BookingCard/BookingCard";
+import { Alert } from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import { useAuthContext } from '../../hooks/useAuthContext';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const BookingList = ({
-    bookings
-}) => {
+const BookingList = () => {
+    const { user } = useAuthContext();
+    const getMyBookings = () => {
+        return fetch(`https://localhost:7016/api/Order/Renter/${user.user.renterId}`)
+            .then(res => res.json())
+    }
+
+    const {
+        data : bookings,
+        isError,
+        isLoading,
+        isFetching
+      } = useQuery(['getMyBookingsKey'], getMyBookings, {
+        retry: false,
+        onError: () => toast.error('Something went wrong!'),
+        refetchOnWindowFocus: false,
+      })
+
+
     return (
-        <>
-            {bookings.length > 0
-                ? (
-                    <ul className="other-bookings-list">
-                        {bookings.map(x => <BookingCard key={x.id} booking={x} />)}
-                    </ul>
-                ) 
-                : <p className="no-bookings">You do not have any bookings!</p>
-            }
-        </>
-    );
-}
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Make & Model</TableCell>
+            <TableCell align="right">Reg number</TableCell>
+            <TableCell align="right">Pick up date&time</TableCell>
+            <TableCell align="right">Drop off date&time</TableCell>
+            <TableCell align="right">Duration</TableCell>
+            <TableCell align="right">Pick up location</TableCell>
+            <TableCell align="right">Drop off location</TableCell>
+            <TableCell align="right">Insurance</TableCell>
+            <TableCell align="right">Payment type</TableCell>
+            <TableCell align="right">Total amount</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+      {isError && <Alert severity="error">This is an error alert — check it out!</Alert>}
+      {!isLoading && !isFetching && !isError && bookings &&
+          bookings.map((booking) => (
+            <TableRow
+              key={booking.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {booking.carMake} {booking.carModel} 
+              </TableCell>
+              <TableCell align="right">{booking.regNumber}</TableCell>
+              <TableCell align="right">{booking.pickUpDateAndTime}</TableCell>
+              <TableCell align="right">{booking.dropOffDateAndTime}</TableCell>
+              <TableCell align="right">{booking.duration}</TableCell>
+              <TableCell align="right">{booking.pickUpLocation}</TableCell>
+              <TableCell align="right">{booking.dropOffLocation}</TableCell>
+              <TableCell align="right">{booking.insurance ? 'Yes' : 'No'}</TableCell>
+              <TableCell align="right">{booking.paymentType}</TableCell>
+              <TableCell align="right">{booking.totalAmount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+);
+};
 
 export default BookingList;
